9ece2846d88ade3bfdebcc6430d2a8c796a7ab18
weishian-wang <weishianwang@gmail.com>
2019-01-31 16:02:15 -0500

    Add background particles animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 450
+      }
+    }
+  }
+}
 
 class App extends Component {
   constructor() {
@@ -20,6 +33,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation isSignedIn={isSignedIn} />
         <Logo />
       </div>
