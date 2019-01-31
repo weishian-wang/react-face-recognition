f5acb4e6208c87569041216ccb3612221f5c3c4b
weishian-wang <weishianwang@gmail.com>
2019-01-30 20:25:55 -0500

    Updates

---
+++ precache-manifest.05ea1566534734bf2271bb96795f9070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-face-recognition/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a5d5ea8ef6a7078db61",
+    "url": "/react-face-recognition/static/js/runtime~main.2a5d5ea8.js"
+  },
+  {
+    "revision": "2119efe2a375fa07f689",
+    "url": "/react-face-recognition/static/js/main.2119efe2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-face-recognition/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2119efe2a375fa07f689",
+    "url": "/react-face-recognition/static/css/main.7706bd77.chunk.css"
+  },
+  {
+    "revision": "7edf6ff7c3a36e2b1261145262e086dd",
+    "url": "/react-face-recognition/index.html"
+  }
+];
