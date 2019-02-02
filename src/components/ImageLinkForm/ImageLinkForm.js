1fe51b60b5bd8b84dc3be9309d52fcdd46d7e389
weishian-wang <weishianwang@gmail.com>
2019-02-02 16:16:51 -0500

    Update ImageLinkForm description

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,6 +7,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 			<p className='f3'>
 				{'This Magic Brain will detect faces in your pictures. Give it a try : )'}
 			</p>
+			<p className='f3'>
+				{'Please enter a publicly accessible URL of image:'}
+			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
 					<input className='f4 pa2 w-80 center' type='text' onChange={onInputChange} />
