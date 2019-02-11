33224d668ba4bb4291ed93545352f462e5799bf0
weishian-wang <weishianwang@gmail.com>
2019-02-11 16:39:19 -0500

    Add Signin form validation

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { FormValidation } from 'calidation';
+
+const config = {
+  email: {
+    isRequired: 'Email address is required',
+    isEmail: 'Please enter a valid email'
+  },
+  password: {
+    isRequired: 'Password is also required',
+    isMinLength: {
+      message: 'Password must at least be 8 characters long',
+      length: 8
+    }
+  }
+};
 
 class Signin extends Component {
   constructor(props) {
@@ -17,23 +32,25 @@ class Signin extends Component {
     this.setState({ password: event.target.value });
   };
 
-  onSubmitSignIn = () => {
-    fetch('http://localhost:8080/signin', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password
+  onSubmitSignIn = ({ errors, fields, isValid }) => {
+    if (isValid) {
+      fetch('http://localhost:8080/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password
+        })
       })
-    })
-      .then(res => res.json())
-      .then(userData => {
-        if (userData.id) {
-          this.props.loadUser(userData);
-          this.props.onRouteChange('home');
-        }
-      })
-      .catch(err => console.log(err));
+        .then(res => res.json())
+        .then(userData => {
+          if (userData.id) {
+            this.props.loadUser(userData);
+            this.props.onRouteChange('home');
+          }
+        })
+        .catch(err => console.log(err));
+    }
   };
 
   render() {
@@ -42,51 +59,64 @@ class Signin extends Component {
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f2 fw6 ph0 mh0">Sign In</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                  Email
-                </label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  onChange={this.onEmailChange}
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={this.onPasswordChange}
-                />
-              </div>
-            </fieldset>
-            <div>
-              <input
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Sign in"
-                onClick={this.onSubmitSignIn}
-              />
-            </div>
-            <div className="lh-copy mt3">
-              <p
-                className="f6 link dim black db pointer"
-                onClick={() => onRouteChange('register')}
-              >
-                Register
-              </p>
-            </div>
-          </div>
+          <FormValidation
+            config={config}
+            onSubmit={this.onSubmitSignIn}
+            className="measure"
+          >
+            {({ errors, submitted }) => (
+              <Fragment>
+                <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
+                  <legend className="f2 fw6 ph0 mh0">Sign In</legend>
+                  <div className="mt3">
+                    <label className="db fw6 lh-copy f6" htmlFor="email">
+                      Email
+                    </label>
+                    <input
+                      className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                      type="email"
+                      name="email"
+                      id="email"
+                      onChange={this.onEmailChange}
+                    />
+                    {submitted && errors.email && (
+                      <span className="dark-red fw7">{errors.email}</span>
+                    )}
+                  </div>
+                  <div className="mv3">
+                    <label className="db fw6 lh-copy f6" htmlFor="password">
+                      Password
+                    </label>
+                    <input
+                      className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                      type="password"
+                      name="password"
+                      id="password"
+                      onChange={this.onPasswordChange}
+                    />
+                    {submitted && errors.password && (
+                      <span className="dark-red fw7">{errors.password}</span>
+                    )}
+                  </div>
+                </fieldset>
+                <div>
+                  <input
+                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                    type="submit"
+                    value="Sign in"
+                  />
+                </div>
+                <div className="lh-copy mt3">
+                  <p
+                    className="f6 link dim black db pointer"
+                    onClick={() => onRouteChange('register')}
+                  >
+                    Register
+                  </p>
+                </div>
+              </Fragment>
+            )}
+          </FormValidation>
         </main>
       </article>
     );
