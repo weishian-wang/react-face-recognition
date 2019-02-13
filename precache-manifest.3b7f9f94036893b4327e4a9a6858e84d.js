879a19e7f9f67c6cb39255b2204818b92baa04e4
weishian-wang <weishianwang@gmail.com>
2019-02-13 16:37:16 -0500

    Updates

--- precache-manifest.3b7f9f94036893b4327e4a9a6858e84d.js
+++ precache-manifest.3b7f9f94036893b4327e4a9a6858e84d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-face-recognition/static/js/runtime~main.2a5d5ea8.js"
   },
   {
-    "revision": "a3eaaaf92688f864cd8a",
-    "url": "/react-face-recognition/static/js/main.a3eaaaf9.chunk.js"
+    "revision": "72a82412ed91ba154fae",
+    "url": "/react-face-recognition/static/js/main.72a82412.chunk.js"
   },
   {
     "revision": "8cce0ac19b9f832d9acb",
     "url": "/react-face-recognition/static/js/1.8cce0ac1.chunk.js"
   },
   {
-    "revision": "a3eaaaf92688f864cd8a",
+    "revision": "72a82412ed91ba154fae",
     "url": "/react-face-recognition/static/css/main.04fa374b.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-face-recognition/static/css/1.862ccef7.chunk.css"
   },
   {
-    "revision": "ffb038d879d612eb4c7378af97b66934",
+    "revision": "f538b7a3004eff6bd708f70e45510798",
     "url": "/react-face-recognition/index.html"
   }
 ];
